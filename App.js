0823d4050b59dd3ec3569c9daf14b744a7abedb5
AbdHussein <abdulrahmaanhussein97@gmail.com>
2020-10-21 12:09:59 +0300

    V 1.0

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+
 
 export default function App() {
+  var [textColor, setTextColor] = useState('#000');
+
+  function changeColor(){  
+    const red = Math.floor(Math.random() * 255);
+    const blue = Math.floor(Math.random() * 255);
+    const green = Math.floor(Math.random() * 255);
+    setTextColor(`rgb(${red},${green},${blue})`);
+    console.log(textColor);
+    return;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{color: textColor, fontSize: 50}}>Hello World!!</Text>
+      <Button title={"Press Me!"} style={styles.button} onPress={changeColor}></Button>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +32,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button:{
+    borderStyle:"solid",
+    borderColor:"#000",
+    borderRadius: 2,
+    borderWidth: 2
+  }
 });
